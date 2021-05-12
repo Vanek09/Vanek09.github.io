ef7436cc73235fa3831c393baae03b235d59c422
Vanek09 <korobka.ivan09@gmail.com>
2021-05-12 23:39:51 +0300

    s

--- script.js
+++ script.js
@@ -4,7 +4,24 @@
  const indicator=document.querySelector(".indicator");
  let index=0;
 
+ $(function() {
+     window.addEventListener('resize', function () {
+         set_100vh_var();
+         detect_mobile();
+     });
+     window.addEventListener('orientationchange', function () {
+         set_100vh_var();
+     });
 
+     setTimeout(
+         set_100vh_var,
+         50
+     );
+ }
+
+ function set_100vh_var() {
+     document.documentElement.style.setProperty('--vh', `${window.innerHeight/100}px`);
+ };
 
 
    // create circle indicators
